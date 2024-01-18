6023b1a22b594af515ac62a3e7757063101261fb
Mare534 <markoilioski21@gmail.com>
2024-01-18 21:23:14 +0100

    First commit. Opens a empty window.

---
+++ first_app.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "lve_window.hpp"
+
+namespace lve{
+    class FirstApp{
+        
+        public:
+            static constexpr int WIDTH=1280;
+            static constexpr int HEIGHT=720;
+
+            void run();
+            
+        private:
+            LveWindow lveWindow{WIDTH, HEIGHT, "Hello Vulkan!"};
+    };
+}
