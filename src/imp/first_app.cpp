61ef1ba6e070b84656278e659a9342a38f11ab09
Mare534 <markoilioski21@gmail.com>
2024-02-04 20:30:34 +0100

    Made io global.

--- src/imp/first_app.cpp
+++ src/imp/first_app.cpp
@@ -45,10 +45,17 @@ namespace lve
 
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO();
+    (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;  // Enable Gamepad Controls
     UseImGui myimgui{lveWindow.getGLFWwindow(), lveDevice, lveRenderer};
 
     auto currentTime = std::chrono::high_resolution_clock::now();
 
+    bool show_demo_window = {true};
+    bool show_another_window{false};
+    ImVec4 clear_color{ImVec4(0.45f, 0.55f, 0.60f, 1.00f)};
     while (!lveWindow.shouldClose())
     {
       glfwPollEvents();
@@ -78,6 +85,27 @@ namespace lve
 
         lveRenderer.beginSwapChainRenderPass(commandBuffer);
         myimgui.NewFrame();
+        {
+          static float f = 0.0f;
+          static int counter = 0;
+
+          ImGui::Begin("Hello, world!"); // Create a window called "Hello, world!" and append into it.
+
+          ImGui::Text("This is some useful text.");          // Display some text (you can use a format strings too)
+          ImGui::Checkbox("Demo Window", &show_demo_window); // Edit bools storing our window open/close state
+          ImGui::Checkbox("Another Window", &show_another_window);
+
+          ImGui::SliderFloat("float", &f, 0.0f, 1.0f);             // Edit 1 float using a slider from 0.0f to 1.0f
+          ImGui::ColorEdit3("clear color", (float *)&clear_color); // Edit 3 floats representing a color
+
+          if (ImGui::Button("Button")) // Buttons return true when clicked (most widgets return true when edited/activated)
+            counter++;
+          ImGui::SameLine();
+          ImGui::Text("counter = %d", counter);
+
+          ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+          ImGui::End();
+        }
         myimgui.Update();
         simpleRenderSystem.renderGameObjects(commandBuffer, gameObjects, camera);
         myimgui.Render(commandBuffer);
